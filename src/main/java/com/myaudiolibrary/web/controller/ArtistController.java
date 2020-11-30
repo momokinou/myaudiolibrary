1827e74e8d9956b845b07117254eeffcd764e737
qmoriceau <quentin.moriceau@ipilyon.net>
2020-11-30 22:33:37 +0100

    manage exercices 3/4/5

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -35,8 +36,35 @@ public class ArtistController {
         return artistRepository.findByName(name, pageRequest);
     }
 
-    public Page<Artist> listArtists(){
-        return null;
+    //Liste des artistes
+    @GetMapping(
+            value = "",
+            produces = "application/json",
+            params = {"page", "size", "sortProperty", "sortDirection"})
+    public Page<Artist> listArtists(@RequestParam (value = "page", defaultValue = "0") Integer page,
+                                    @RequestParam (value = "size", defaultValue = "10") Integer size,
+                                    @RequestParam (value = "sortProperty") String sortProperty,
+                                    @RequestParam (value = "sortDirection") String sortDirection){
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty);
+        return artistRepository.findAll(pageRequest);
     }
 
+    //Création d'un artiste
+    @PostMapping(
+            value = "",
+            produces = "application/json",
+            consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Artist newArtist(@RequestBody Artist artist) {
+        return artistRepository.save(artist);
+    }
+
+    //Modification d'un artiste
+    @PutMapping(
+            value = "/{id}",
+            consumes = "application/json",
+            produces = "application/json")
+    public Artist updateArtist(@PathVariable Integer id, @RequestBody Artist artist){
+        return artistRepository.save(artist);
+    }
 }
